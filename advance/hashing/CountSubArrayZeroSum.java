4de527a8d612e549a79e2e0a111238a1af8dde3c
sarang.kaliyath <sarang.kaliyath@sakhaglobal.com>
2024-09-13 19:13:33 +0530

    hashing 1

---
+++ advance/hashing/CountSubArrayZeroSum.java
@@ -0,0 +1,103 @@
+package advance.hashing;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashMap;
+
+/*
+Problem Description
+Given an array A of N integers.
+Find the count of the subarrays in the array which sums to zero.
+Since the answer can be very large, return the remainder on dividing the result with 10^9+7
+
+Problem Constraints
+1 <= N <= 10^5
+-109 <= A[i] <= 10^9
+
+Input Format
+Single argument which is an integer array A.
+
+Output Format
+Return an integer.
+
+Example Input
+Input 1: A = [1, -1, -2, 2]
+Input 2: A = [-1, 2, -1]
+
+Example Output
+Output 1: 3
+Output 2: 1
+
+Example Explanation
+
+Explanation 1:
+ The subarrays with zero sum are [1, -1], [-2, 2] and [1, -1, -2, 2].
+
+Explanation 2:
+ The subarray with zero sum is [-1, 2, -1].
+
+ */
+public class CountSubArrayZeroSum {
+
+    public  static  void main (String [] args){
+
+        ArrayList<Integer> A = new ArrayList<>(Arrays.asList(-1, 2, -1));
+        int n = A.size();
+
+        bruteForce(A, n);
+
+        optimized(A);
+    }
+
+    public  static void bruteForce (ArrayList<Integer> A, int n){
+
+        int count = 0;
+
+        for(int i = 0; i < n ; i++){
+
+            int sum = 0;
+
+            for(int j = i; j < n; j++){
+
+                sum+= A.get(j);
+
+                if(sum == 0) count++;
+
+            }
+
+        }
+
+        System.out.println(count);
+
+    }
+
+    public  static  void optimized(ArrayList<Integer> A){
+
+        HashMap<Integer, Integer> hm = new HashMap<>();
+        int mod = (int) Math.pow(10, 9) + 7;
+        int count = 0;
+
+        int sum = 0;
+
+        hm.put(0, 1);
+
+        for(int el : A){
+
+            sum += el;
+
+            if(hm.containsKey(sum)){
+                count = (count + hm.get(sum)) % mod;
+            }
+            else {
+                hm.put(sum, 0);
+            }
+
+            hm.put(sum, hm.get(sum) + 1);
+
+        }
+
+        System.out.println(count);
+    }
+}
