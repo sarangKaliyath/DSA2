80ddd7b4783468edaea26e4651d11133ee08ed47
sarang.kaliyath <sarang.kaliyath@sakhaglobal.com>
2024-09-14 15:05:32 +0530

    change

--- advance/hashing/SubArrayWithZeroSum.java
+++ advance/hashing/SubArrayWithZeroSum.java
@@ -10,7 +10,7 @@ Problem Description
 
 Given an array of integers A,
 find and return whether the given array contains a non-empty subArray with a sum equal to 0.
-If the given array contains a sub-array with sum zero return 1, valse return 0.
+If the given array contains a sub-array with sum zero return 1, else return 0.
 
 Problem Constraints
 1 <= |A| <= 100000
